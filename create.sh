9652991d0b6cee2365a20e872a3be2307bffbd84
Jimmy <jimmybchopps@gmail.com>
2023-01-27 06:04:52 -0600

    Updated creation script and added comments

--- create.sh
+++ create.sh
@@ -7,21 +7,18 @@ export TF_VAR_my_ip=$(curl -s https://wtfismyip.com/text)/32
 #Replace Unique ID in AWS Inventory File
 sed -i 's/tag:UUID: .*/tag:UUID: '"$TF_VAR_uuid"'/g' ansible/aws_ec2.yaml
 
-#Start Terraform and Show Plan
-terraform -chdir=terraform init && \
-terraform -chdir=terraform plan
+#Init Terraform and Build the environment
+terraform -chdir=terraform init
+terraform -chdir=terraform apply -auto-approve
 
-#Confirm Before Building and Running Ansible
-echo "Everything look good?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes ) terraform -chdir=terraform apply -auto-approve && exit;;
-    No ) exit;;
-  esac
-done
+#If specifying a large amount of instances consider using parallelism parameter to speed up terraform (example below)
+#terraform -chdir=terraform apply -parallelism 25 -auto-approve
 
 #Change permissions on key
 chmod 400 access/ec2_key.pem
+
+#Set variable so ansible knows where to look for configuration values
+export ANSIBLE_CONFIG=$PWD/ansible/ansible.cfg
 
 #Run the playbook
 ansible-playbook ./ansible/main.yml
